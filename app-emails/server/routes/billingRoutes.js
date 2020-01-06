1458cfef7ae092b997d2f05e653b8e9c2e6bd86c
kaziuz <alex@alex-pc.(none)')>
2020-01-06 13:12:09 -0500

    delete app node

--- app-emails/server/routes/billingRoutes.js
+++ app-emails/server/routes/billingRoutes.js
@@ -6,20 +6,20 @@ const requireLogin = require('../middlewares/requireLogin')
 // https://stripe.com/docs/api/charges/create?lang=node
 // parseando el request del cliente con body-parser
 
-module.exports = app =>  {
-    app.post('/api/stripe', requireLogin, async (req, res) => {
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: '$5 for 5 credits',
-            source: req.body.id
-        });
-
-        // añadimos al stock de credits del usuario 5 creditos
-        // tomando una copia de este con req.user
-        req.user.credits += 5;
-        const user = await req.user.save();
-
-        res.send(user);
+module.exports = app => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id
     });
+
+    // añadimos al stock de credits del usuario 5 creditos
+    // tomando una copia de este con req.user
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
+  });
 }
